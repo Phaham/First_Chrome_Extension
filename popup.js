69371e21c692ba8684bfdfd9d0415357e1191b55
PHAHAM KHAN <72141945+Phaham@users.noreply.github.com>
2022-11-27 12:18:09 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,3 @@
-
-// async function getCurrentTab() {
-//   let queryOptions = { active: true, lastFocusedWindow: true };
-//   // `tab` will either be a `tabs.Tab` instance or `undefined`.
-//   let [tab] = await chrome.tabs.query(queryOptions);
-//   return tab;
-// }
-
-// const tab = getCurrentTab();
 const button = document.getElementById("check");
 const tabTitle = document.querySelector('.title');
 const taburl = document.querySelector('.url');
@@ -23,6 +14,35 @@ button.addEventListener("click", async () => {
       taburl.innerHTML = geturl;
     });
 });
+
+const update_url = (arr) => {
+  setTimeout(function () {
+    for (let i = 0; i < arr.length; i++) {
+      chrome.tabs.update(
+        { url: arr[i] }
+      );
+    }
+  }, 5000);
+}
+
+
+button.addEventListener("click", () => {
+  let input = document.getElementById("links").value;
+  let Links_arr = input.substring(1, input.length - 1).split(",");
+
+  let link = [];
+
+  Links_arr.forEach(user_link => {
+    let exact_url = user_link.trim();
+
+    link.push(exact_url.substring(1, exact_url.length - 1))
+    link.push(exact_url.substring(1, exact_url.length - 1) + "/overlay/contact-info")
+  })
+
+  update_url(link);
+});
+
+// Array Of LinkedIn Profiles - ["https://www.linkedin.com/in/isanur-sardar/", "https://www.linkedin.com/in/mohd-ajmal-6ab006217/","https://www.linkedin.com/in/mohd-saif-b62a61201/"]
 
 document.addEventListener('DOMContentLoaded', () => {
   wrapper.classList.remove('hidden');
