fe54d587e5f83bed3a227ee3d6096657a9010e1e
PHAHAM KHAN <72141945+Phaham@users.noreply.github.com>
2022-12-03 19:34:30 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,21 +1,75 @@
-let username = document.querySelector("h1.text-heading-xlarge").textContent;
-let user_location = document.querySelector("span.text-body-small.inline").textContent;
 
+let likeCount, commentCount;
+chrome.runtime.onMessage.addListener((count, sender, sendResponse) => {
+  sendResponse({ msg: "Recived" });
+  console.log(count);
+  likeCount = count.likeData;
+  commentCount = count.commentData;
 
-if (window.location.href.includes("/overlay/contact-info") === true) {
-  let contact_info = document.getElementsByClassName("pv-contact-info__ci-container")
-  var arr = []
-  for (let i = 0; i < contact_info.length; i++) {
-    arr.push(contact_info[i].innerText)
-  }
-  chrome.runtime.sendMessage({ contact_info: arr }, () => {
-    for (let i = 0; i < contact_info.length; i++) {
-      console.log((contact_info[i].innerText));
-    }
-  });
-}
-
-chrome.runtime.sendMessage({ username, user_location }, () => {
-  console.log(`username: ${username}`);
-  console.log(`address: ${user_location}`);
+  react();
+  // like(0);
+  // comment(0);
 });
+
+
+
+let i = 0, j = 0;
+function react() {
+
+  if (i < likeCount) {
+    let like = document.querySelector('.scaffold-finite-scroll__content').querySelectorAll('div .feed-shared-social-action-bar__action-button .react-button__trigger')[i];
+    console.log(like);
+
+    like.click();
+    i++;
+  }
+  if (j < commentCount) {
+    let commentbtn = document.querySelector('.scaffold-finite-scroll__content').querySelectorAll('div .comment span div button')[j]
+    commentbtn.click()
+    let comment = document.querySelector('.scaffold-finite-scroll__content').querySelectorAll('div .feed-shared-update-v2__comments-container .comments-comment-box .comments-comment-box__form-container .comments-comment-texteditor .comments-comment-box-comment__text-editor .editor-container .editor-content .ql-editor p')[j];
+    comment.innerHTML = "CFBR";
+    let post = document.querySelector('.scaffold-finite-scroll__content').querySelectorAll('div .social-details-social-activity .feed-shared-update-v2__comments-container .comments-comment-box .comments-comment-box__form-container form')[j].querySelectorAll('div button')[2]
+    post.click();
+    j++;
+  }
+}
+setInterval(react, 4000);
+
+
+// function like(i) {
+//   if (i < likeCount) {
+//     // if (i >0) {
+//     setTimeout(() => {
+//       let likebtn = document.querySelector('.scaffold-finite-scroll__content').querySelectorAll('div .feed-shared-social-action-bar__action-button .react-button__trigger')[i];
+//       console.log(likebtn);
+
+//       likebtn.click();
+//       like(++i);
+//       // like(i--);
+//     }, 3000);
+//   } else {
+//     return
+//   }
+// };
+
+// function comment(j) {
+//   if (i < commentCount) {
+//     setTimeout(() => {
+//       let commentbtn = document.querySelector('.scaffold-finite-scroll__content').querySelectorAll(".comment-button")[i];
+//       commentbtn.click();
+
+//       setTimeout(()=>{
+//       let commentText = document.querySelectorAll(".editor-content p")[i];
+//       commentText.innerHTML = "CFBR";
+//       },500);
+//       let commentSubmit = document.querySelector(".comments-comment-box__submit-button")[i];
+//       commentSubmit.click();
+
+//       comment(++i);
+//     }, 3000);
+//   } 
+
+//   else {
+//     return
+//   }
+// };
