eb107d37e78b13db7b4ae209de9d18fe4bc7599b
PHAHAM KHAN <72141945+Phaham@users.noreply.github.com>
2022-12-03 19:34:58 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,47 +2,60 @@ const button = document.getElementById("check");
 const tabTitle = document.querySelector('.title');
 const taburl = document.querySelector('.url');
 const wrapper = document.querySelector('.wrapper');
+const disabled_btn = document.querySelector('.disabled');
 
-button.addEventListener("click", async () => {
-  chrome.tabs.query(
-    { "currentWindow": true, "active": true },
-    function (tabs) {
-      let gettitle = tabs[0].title;
-      let geturl = tabs[0].url;
+const input = document.querySelectorAll('.input-field');
+const like = document.querySelector('.like');
+const comment = document.querySelector('.comment');
 
-      tabTitle.innerHTML = gettitle;
-      taburl.innerHTML = geturl;
+function enable() {
+  button.disabled = false;
+  button.classList.remove('disabled');
+}
+
+function disable() {
+  button.disabled = true;
+  button.classList.add('disabled');
+}
+
+like.addEventListener('keyup', function () {
+  if (like.value != "" && comment.value != "") {
+    enable();
+  } else {
+    disable();
+  }
+})
+comment.addEventListener('keyup', function () {
+  if (like.value != "" && comment.value != "") {
+    enable();
+  } else {
+    disable();
+  }
+})
+
+
+const sendData = async (data) => {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  chrome.tabs.query(queryOptions, async (tab) => {
+    chrome.tabs.sendMessage(tab[0].id, data, async (response) => {
+      console.log(await response);
     });
-});
-
-const update_url = (arr) => {
-  setTimeout(function () {
-    for (let i = 0; i < arr.length; i++) {
-      chrome.tabs.update(
-        { url: arr[i] }
-      );
-    }
-  }, 5000);
+  });
 }
 
 
-button.addEventListener("click", () => {
-  let input = document.getElementById("links").value;
-  let Links_arr = input.substring(1, input.length - 1).split(",");
+button.addEventListener("click", async () => {
+  let likeData = parseInt(like.value);
+  let commentData = parseInt(comment.value);
 
-  let link = [];
+  let data = {
+    likeData: likeData,
+    commentData: commentData
+  }
 
-  Links_arr.forEach(user_link => {
-    let exact_url = user_link.trim();
-
-    link.push(exact_url.substring(1, exact_url.length - 1))
-    link.push(exact_url.substring(1, exact_url.length - 1) + "/overlay/contact-info")
-  })
-
-  update_url(link);
+  await sendData(data);
 });
 
-// Array Of LinkedIn Profiles - ["https://www.linkedin.com/in/isanur-sardar/", "https://www.linkedin.com/in/mohd-ajmal-6ab006217/","https://www.linkedin.com/in/mohd-saif-b62a61201/"]
 
 document.addEventListener('DOMContentLoaded', () => {
   wrapper.classList.remove('hidden');
