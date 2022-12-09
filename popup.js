ac3ae8760c29ca7dca8303f4891b23a0097ae0ab
PHAHAM KHAN <72141945+Phaham@users.noreply.github.com>
2022-12-09 21:51:32 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,61 +1,78 @@
 const button = document.getElementById("check");
-const tabTitle = document.querySelector('.title');
-const taburl = document.querySelector('.url');
 const wrapper = document.querySelector('.wrapper');
-const disabled_btn = document.querySelector('.disabled');
 
-const input = document.querySelectorAll('.input-field');
-const like = document.querySelector('.like');
-const comment = document.querySelector('.comment');
+const overlay = document.querySelector('.popup')
+const start = document.querySelector('.start');
+const stop = document.querySelector('.stop');
 
-function enable() {
-  button.disabled = false;
-  button.classList.remove('disabled');
-}
+const video = document.querySelector('.video');
 
-function disable() {
-  button.disabled = true;
-  button.classList.add('disabled');
-}
-
-like.addEventListener('keyup', function () {
-  if (like.value != "" && comment.value != "") {
-    enable();
-  } else {
-    disable();
-  }
-})
-comment.addEventListener('keyup', function () {
-  if (like.value != "" && comment.value != "") {
-    enable();
-  } else {
-    disable();
-  }
-})
-
-
-const sendData = async (data) => {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  chrome.tabs.query(queryOptions, async (tab) => {
-    // chrome.tabs.update(tab.id, {url: 'https://www.linkedin.com/feed/'});
-    chrome.tabs.sendMessage(tab[0].id, data, async (response) => {
-      console.log(await response);
-    });
-  });
-}
+const EXTENSION_ID = 'cglldpiidbaijkonhmcmjgfienagpijd';
+const request = { sources: ['tab'] };
+let stream;
 
 
 button.addEventListener("click", async () => {
-  let likeData = parseInt(like.value);
-  let commentData = parseInt(comment.value);
-
-  let data = {
-    likeData: likeData,
-    commentData: commentData
-  }
-
-  await sendData(data);
+  button.classList.add('hidden');
+  overlay.classList.remove('hidden');
 });
+
+start.addEventListener('click', () => {
+  chrome.runtime.sendMessage(EXTENSION_ID, request, response => {
+
+    if (response && response.type === 'success') {
+      navigator.mediaDevices.getDisplayMedia({
+        video: {
+          mandatory: {
+            chromeMediaSource: 'desktop',
+            chromeMediaSourceId: response.streamId,
+          }
+        }
+      }).then(returnedStream => {
+        stream = returnedStream;
+        video.src = URL.createObjectURL(stream);
+        start.innerHTML = 'STOP';
+      }).catch(err => {
+        console.error('Could not get stream: ', err);
+      });
+    } else {
+      console.error('Could not get stream');
+    }
+
+  });
+});
+
+
+// Method - 2
+
+// start.addEventListener('click', () => {
+//   chrome.runtime.sendMessage(sender, request, response => {
+//     const startCapture = async () => {
+//       const stream = await navigator.mediaDevices.getDisplayMedia({
+//         video: {
+//           mediaSource: "tab",
+//         }
+//       })
+//       startCapture();
+//       const data = [];
+//       const mediaRecorder = new MediaRecorder(stream);
+
+//       mediaRecorder.ondataavailable = (e) => {
+//         data.push(e.data);
+//       };
+//       start.addEventListener('click', (e) => {
+//         mediaRecorder.start();
+//       })
+
+//       stop.addEventListener('click', (e) => {
+//         video.src = URL.createObjectURL(new Blob(data, {
+//           type: data[0].type,
+//         }))
+//       })
+//     }
+//   });
+// });
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
